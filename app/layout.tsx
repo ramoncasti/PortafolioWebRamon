8f704c76fb62f14657b4f48b9fd7083c41eda9ae
Ramón López Castiñeira <84033537+ramoncasti@users.noreply.github.com>
2025-12-01 00:03:49 -0300

    cambios en bootstrap y css

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,19 @@ import Footer from "./components/Footer";
 
 export const metadata = {
   title: "Portafolio | Ramón López",
-  description: "Portafolio personal de Ramón López",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
-      <head></head>
       <body>
         {children}
         <Footer />
 
-        {/* Bootstrap JS debe ir aquí */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           defer
